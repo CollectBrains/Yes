3b2d5b75706ce12db84102d8109f3adedcc7d278
CollectBrains <ruzanow.dmitry@gmail.com>
2018-06-21 07:03:40 +0300

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,15 @@
 $(function() {
 
-	// Custom JS
+	var swiper = new Swiper('.swiper-container', {
+
+		slidesPerView: 1,
+		spaceBetween: 30,
+		mousewheel: true,
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			hide: false,
+		},
+	});
 
 	svg4everybody();
 
